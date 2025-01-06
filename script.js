d309aae8a5ad59342bbcdbe62036876e0a68d6e5
Garima Mishra <garima26062005@gmail.com>
2025-01-07 02:43:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
 
+buttons.forEach(function (button){
+    console.log(button);
+    button.addEventListener('click', function (e) {
+        console.log(e);
+        console.log(e.target);
+        if(e.target.id === 'grey'){
+            body.style.background = e.target.id;
+        }
+        if(e.target.id === 'pink'){
+            body.style.background = e.target.id;
+        }
+        if(e.target.id === 'red'){
+            body.style.background = e.target.id;
+        }
+        if(e.target.id === 'purple'){
+            body.style.background = e.target.id;
+        }
+
+    });
+});
